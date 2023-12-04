b8386f37070d906a99e516ccbe74860eb9661c17
sandraa450 <151436215+sandraa450@users.noreply.github.com>
2023-12-04 22:46:11 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,35 +1,27 @@
 <?php
+$un = $_POST["username"];
+$pw = $_POST["password"];
 
-    $un =$_POST["username"];
-    $pw=$_POST["password"];
+$file = 'users.json';
+$users = json_decode(file_get_contents($file), true) ?? [];
+$userExists = false;
 
-    $file = 'users.json';
-    $users = json_decode(file_get_contents($file), true) ?? [];
-    if (!file_exists($file) || empty($users)) {
-        header('Location: signup.html');
-        exit;
+foreach ($users as $user) {
+    if ($user['username'] === $un && $user['password'] === $pw) {
+        $userExists = true;
+        break;
     }
+}
 
-    
-    $userexist = false;
-    foreach ($users as $userexists) {
-        if ($userexists['username'] === $un) {
-            $userexist = true;
-            break;
-        }
-    }
-
-    if ($userexist) {
-        session_start();
-        $_SESSION["username"]=ucfirst($un);
-        header('Location: main.php');
-        exit;
-    } else {
-        echo ("Login failed!");
-            
-        
-        echo '<script>window.location.href = "index.php";</script>';
-        exit;
-    }
-    
+if ($userExists) {
+    session_start();
+    $_SESSION["username"] = ucfirst($un);
+    header('Location: main.php');
+    exit;
+} else {
+    $errorMessage = "Incorrect username or password. Please try again.";
+    echo "<script>alert('$errorMessage');</script>";
+    echo '<script>window.location.href = "index.php";</script>';
+    exit;
+}
 ?>
